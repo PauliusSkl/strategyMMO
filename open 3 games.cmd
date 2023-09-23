c634e955f845fcee824090cdd8fffb4a2d13c072
lukas.veckys <58853955+phlukas@users.noreply.github.com>
2023-09-23 15:20:34 +0300

    created cmd file

---
+++ open 3 games.cmd
@@ -0,0 +1,5 @@
+@echo off
+start "" ".\WarGame\bin\Debug\net6.0-windows\WarGame.Forms.exe"
+start "" ".\WarGame\bin\Debug\net6.0-windows\WarGame.Forms.exe"
+start "" ".\WarGame\bin\Debug\net6.0-windows\WarGame.Forms.exe"
+exit
