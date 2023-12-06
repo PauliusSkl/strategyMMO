955007dd6fed44f4cc540a6aeb61da3c1ca15e7c
lukas.veckys <58853955+phlukas@users.noreply.github.com>
2023-12-06 09:13:30 +0200

    added one more visitor

---
+++ Shared/Models/Visitor/UnitHealVisitor.cs
@@ -0,0 +1,24 @@
+﻿namespace Shared.Models.Visitor;
+
+public class UnitHealVisitor : IVisitor
+{
+    public void VisitArcher(Archer archer)
+    {
+        archer.Health++;
+    }
+
+    public void VisitMage(Mage mage)
+    {
+        mage.Health += 2;
+    }
+
+    public void VisitTank(Tank tank)
+    {
+        tank.Health += 3;
+    }
+
+    public void VisitWarrior(Warrior warrior)
+    {
+        warrior.Health += 4;
+    }
+}
